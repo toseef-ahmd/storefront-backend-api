f5d15b12ac33ab7c59dfcff1200e1b2fa709d22a
toseefdc360 <toseef@afnanilab.com>
2022-03-20 11:19:04 +0500

    users model

--- src/models/users.model.ts
+++ src/models/users.model.ts
@@ -1,13 +1,27 @@
-import { QueryResult } from "pg";
+import { PoolClient, QueryResult, ResultBuilder } from "pg";
 import Client from "../database/database";
 import bcrypt from 'bcrypt'
 import { User } from "../interfaces/users.interface";
 
 export class UserModel {
+    async authenticate (email : string, password : string) : Promise<User> {
+        const sql : string = 'SELECT * FROM users WHERE email=$1'
+        const conn : PoolClient = await Client.connect();
+        const result : QueryResult<User> = await Client.query(sql, [email]);
+        const {rows} = result
+        
+       if (rows.length > 0) {
+        const user : User = rows[0];
+        const pepper : string = process.env.PASSWORD_HASH as string;
+        if(bcrypt.compareSync(password + pepper, user.password))
+
+       } 
+    }
+
     async index () : Promise<User[]> {
         try {
-            const conn = await Client.connect();
-            const sql = 'SELECT * FROM users';
+            const conn : PoolClient = await Client.connect();
+            const sql : string = 'SELECT * FROM users';
             const result : QueryResult<User> = await conn.query(sql);
             conn.release()
 
@@ -20,11 +34,11 @@ export class UserModel {
 
     async  create(user:User) : Promise<User> {
       try {
-        const conn = await Client.connect();
-        const sql = 'INSERT INTO USER (email, password, phone, username) VALUES ($1, $2, $3, $4)'
-        const pepper : string = 'pepper'
-        const hashSteps = process.env.SALT_ROUNDS as string
-        const hash = bcrypt.hashSync(user.password + pepper, parseInt(hashSteps));
+        const conn : PoolClient= await Client.connect();
+        const sql : string = 'INSERT INTO USER (email, password, phone, username) VALUES ($1, $2, $3, $4)'
+        const pepper : string = process.env.PASSWORD_HASH as string
+        const salt : string = process.env.SALT_ROUNDS as string
+        const hash : string = bcrypt.hashSync(user.password + pepper, parseInt(salt));
 
         const result : QueryResult<User> = await conn.query(sql, [user.email, hash, user.phone, user.username]);
 
@@ -34,11 +48,10 @@ export class UserModel {
       }
    }
 
-
     async show(id: number): Promise<User> {
         try {
-            const sql = 'SELECT * FROM users WHERE id=($1)'
-            const conn = await Client.connect();
+            const sql : string = 'SELECT * FROM users WHERE id=$1'
+            const conn : PoolClient = await Client.connect();
             const result : QueryResult<User> = await conn.query(sql, [id]);
             conn.release();
             
@@ -51,8 +64,8 @@ export class UserModel {
 
     async delete(id : Number) : Promise<User> {
         try {
-            const sql = 'DELETE * FROM users WHERE id=($1)'
-            const conn = await Client.connect();
+            const sql : string = 'DELETE FROM users WHERE id=$1'
+            const conn : PoolClient = await Client.connect();
             const result : QueryResult<User> = await conn.query(sql, [id]);
             
             conn.release();
