017c8984b7280714ac18a8bd002f48d2cd61427d
toseefdc360 <toseef@afnanilab.com>
2022-05-29 19:51:40 +0500

    updated server.ts

--- src/server.ts
+++ src/server.ts
@@ -8,11 +8,25 @@ const port: number = process.env.PORT as unknown as number || 8000
 
 
 const corsOptions = {
-  origin: ["http://localhost:3000", "http://localhost:4200"],
+  origin: '*',
   optionsSuccessStatus: 200,
 }
 
 app.use(cors(corsOptions))
+app.use(function (req, res, next) {
+
+  // Website you wish to allow to connect
+  res.setHeader('Access-Control-Allow-Origin', '*');
+
+  // Request methods you wish to allow
+  res.setHeader('Access-Control-Allow-Methods', 'GET, POST, OPTIONS, PUT, PATCH, DELETE');
+
+  // Request headers you wish to allow
+  res.setHeader('Access-Control-Allow-Headers', 'X-Requested-With,content-type');
+  // Pass to next layer of middleware
+  next();
+});
+
 app.use(bodyParser.json())
 
 //Calls the Base Routes through Books handler.
